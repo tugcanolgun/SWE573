718a99d40cf9ee1b4190c73d3df4ec4ea965edfb
tugcanolgun <tugcanolgun@gmail.com>
2018-05-07 18:58:22 +0000

    Update gitignore, requirements

--- cryptocial/cryptocial/urls.py
+++ cryptocial/cryptocial/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
